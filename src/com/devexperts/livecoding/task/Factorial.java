6627e8676c704173b2f4e1892536076a9ff219fd
Pavel Smirnov <psmirnov@devexperts.com>
2022-09-05 22:37:11 +0300

    Add Factorial

---
+++ src/com/devexperts/livecoding/task/Factorial.java
@@ -0,0 +1,16 @@
+package com.devexperts.livecoding.task;
+
+// n! = n * (n - 1) * ... * 1
+
+// in:  0, 1, 2, 3,  4, 5
+// out: 1, 1, 2, 6, 24, 120
+
+public class Factorial {
+    /**
+     * Returns factorial of {@code n}, i.e. {@code n!}.
+     */
+    public static int calc(int n) {
+        // type in your cool solution
+        throw new UnsupportedOperationException();
+    }
+}
