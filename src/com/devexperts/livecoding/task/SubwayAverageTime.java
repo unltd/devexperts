179ae853758b5c653650db04ec306e74e2d3296e
Pavel Smirnov <psmirnov@devexperts.com>
2022-09-23 21:37:40 +0300

    ooops

--- src/com/devexperts/livecoding/task/SubwayAverageTime.java
+++ src/com/devexperts/livecoding/task/SubwayAverageTime.java
@@ -8,7 +8,6 @@ public class SubwayAverageTime {
     // 23 A 20
     // 25 A 60
     // 19 A 15
-    // O(1)
     public void checkIn(int id, String stationName, int t) {
         // type in your cool solution
         throw new UnsupportedOperationException();
@@ -17,14 +16,12 @@ public class SubwayAverageTime {
     // 23 C 30
     // 25 C 70
     // 19 D 80
-    // O(1)
     public void checkOut(int id, String stationName, int t) {
         // type in your cool solution
         throw new UnsupportedOperationException();
     }
 
     // A -> C (10 + 6 + 10)/2 = 10
-    // O(1)
     public double getAverageTime(String startStation, String endStation) {
         // type in your cool solution
         throw new UnsupportedOperationException();
