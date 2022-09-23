8cb66055c16e69b229dbf18901660cdfcb00db89
Pavel Smirnov <psmirnov@devexperts.com>
2022-09-23 21:28:29 +0300

    Add SubwayAverageTime

---
+++ src/com/devexperts/livecoding/task/SubwayAverageTime.java
@@ -0,0 +1,32 @@
+package com.devexperts.livecoding.task;
+
+// Stations: A, B, C....
+// Rides:
+// A -> B -> C
+// A -> C != C -> A
+public class SubwayAverageTime {
+    // 23 A 20
+    // 25 A 60
+    // 19 A 15
+    // O(1)
+    public void checkIn(int id, String stationName, int t) {
+        // type in your cool solution
+        throw new UnsupportedOperationException();
+    }
+
+    // 23 C 30
+    // 25 C 70
+    // 19 D 80
+    // O(1)
+    public void checkOut(int id, String stationName, int t) {
+        // type in your cool solution
+        throw new UnsupportedOperationException();
+    }
+
+    // A -> C (10 + 6 + 10)/2 = 10
+    // O(1)
+    public double getAverageTime(String startStation, String endStation) {
+        // type in your cool solution
+        throw new UnsupportedOperationException();
+    }
+}
